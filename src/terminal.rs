da3b390210e8b7155f9c250b3f2969dbc9aec9fd
Jeremy PEPIN <jpepin@student.42.fr>
2016-09-15 07:15:33 +0200

    Use ioctl so far

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,46 +1,38 @@
 use ::libc;
+use ::libc::termios;
+use ::libc::winsize;
 use std::os::unix::io::AsRawFd;
 use std::io::Result;
-use ::winsize;
-use ::termios::*;
 
 use ::pty::prelude as pty;
 
-static mut termios_to_restore: Option<Termios> = None;
+static mut termios_to_restore: Option<termios> = None;
 pub extern "C" fn restore_termios() {
     match unsafe { termios_to_restore } {
-        Some(termios) => { let _ = tcsetattr(libc::STDIN_FILENO, TCSANOW, &termios); }
+        Some(termios) => { let _ = unsafe { libc::ioctl(libc::STDIN_FILENO, (0x80000000 | (116 << 8) | 20 | (((::std::mem::size_of::<termios>() & 0x1FFF) << 16) as u64)), &termios); }; }
         None => ()
     }
 }
 
-pub fn setup_terminal(pty: pty::Master) -> Result<()> {
-    let termios = try!(Termios::from_fd(libc::STDIN_FILENO));
-
-    unsafe {
-        termios_to_restore = Some(termios);
-        libc::atexit(restore_termios);
-    };
-
-    enter_raw_mode(libc::STDIN_FILENO).unwrap();
-
-    let winsize = try!(winsize::from_fd(libc::STDIN_FILENO));
-    winsize::set(pty.as_raw_fd(), &winsize);
-
-    Ok(())
-}
+pub fn setup_terminal(pty: pty::Master) -> Result<termios>
+{ let termios: termios = unsafe { ::std::mem::zeroed() };
+  unsafe { libc::ioctl(libc::STDIN_FILENO, (0x40000000 | (116 << 8) | 19 | (((::std::mem::size_of::<termios>() & 0x1FFF) << 16) as u64)), &termios); }
+  unsafe 
+  { termios_to_restore = Some(termios);
+    libc::atexit(restore_termios); };
+  enter_raw_mode(libc::STDIN_FILENO).unwrap();
+  let winsize: winsize = unsafe { ::std::mem::zeroed() };
+  unsafe { libc::ioctl(libc::STDIN_FILENO, libc::TIOCSWINSZ, winsize); }
+  Ok(termios) }
 
 fn enter_raw_mode(fd: libc::c_int) -> Result<()> {
-    let mut new_termios = try!(Termios::from_fd(fd));
-
-    new_termios.c_lflag ^= !(ICANON);
-    new_termios.c_lflag ^= !(ECHO);
-    new_termios.c_cc[VMIN] = 1;
-    new_termios.c_cc[VTIME] = 0;
-    
-    // MOUSE ON
-    print!("\x1b[?1002h\x1b[?1015h\x1b[?1006h");
-
-    tcsetattr(libc::STDIN_FILENO, TCSANOW, &new_termios);
+    let mut new_termios: termios = unsafe { ::std::mem::zeroed() };
+    unsafe { libc::ioctl(libc::STDIN_FILENO, (0x40000000 | (116 << 8) | 19 | (((::std::mem::size_of::<termios>() & 0x1FFF) << 16) as u64)), &new_termios); }
+    new_termios.c_lflag ^= !(libc::ICANON);
+    new_termios.c_lflag ^= !(libc::ECHO);
+    new_termios.c_cc[libc::VMIN] = 1;
+    new_termios.c_cc[libc::VTIME] = 0;
+    unsafe { libc::ioctl(libc::STDIN_FILENO, (0x80000000 | (116 << 8) | 20 | (((::std::mem::size_of::<termios>() & 0x1FFF) << 16) as u64)), &new_termios); }
+    print!("\x1b[?1002h\x1b[?1015h\x1b[?1006h"); // MOUSE ON
     Ok(())
 }
