592cd8c75961eecc39d056cca65804fbfa3af95e
Jeremy PEPIN <jpepin@student.42.fr>
2016-09-27 06:25:22 +0200

    LR:Add LR Parse for Input

--- src/outp.rs
+++ src/outp.rs
@@ -6,28 +6,31 @@ pub enum Cursor {
   ///-------- Terminal setups ---------
   ///Cursor automatically wraps to next line when it reach the border column
   LineWrap(bool),
-  ///Scrolling enable flag for a definited part of the terminal (flag on/off, index of begin_line, index of end_line)
-  ScrollEnable(bool, libc::c_ushort, libc::c_ushort),
+  ///Enable the scrolling
+  ScrollEnable,
   ///Terminal setups are rebooted to initial state
   TermReset,
 
   ///------- Cursor movements ---------
   ///Cursor moving to new coordinates (column index, line index)
   CursorGoto(libc::c_ushort, libc::c_ushort),
-  ///Cursor is going up
-  CursorUp,
-  ///Cursor is going down
-  CursorDown,
-  ///Cursor is going right
-  CursorRight,
-  ///Cursor is going left
-  CursorLeft,
+  ///Cursor is going up (number of ceils to travel)
+  CursorUp(libc::c_ushort),
+  ///Cursor is going down (number of ceils to travel)
+  CursorDown(libc::c_ushort),
+  ///Cursor is going right (number of ceils to travel)
+  CursorRight(libc::c_ushort),
+  ///Cursor is going left (number of ceils to travel)
+  CursorLeft(libc::c_ushort),
 
   ///------- Position saving ---------
-  ///Cursor position is saved at some coordinates (column index, line index)
-  SaveCursor(libc::c_ushort, libc::c_ushort),
-  ///Cursor position is restored to its saved coordinates (column index, line index)
-  RestoreCursor(libc::c_ushort, libc::c_ushort),
+  ///Save the actual cursor position
+  ///Only one cursor-coordinates can be safe at the same time, each SaveCursor call will replace the oldest coordinates by the newest
+  SaveCursor,
+  ///Cursor position is moved to its safe coordinates
+  ///If no cursor-coordinates was safe with SaveCursor before calling RestoreCursor, this call does nothing
+  ///Many RestoreCursor calls can be done, it will always move the cursor to the safe coordinates
+  RestoreCursor,
 
   ///---------- Scrolling -----------
   ///Scroll the terminal one line up
@@ -35,16 +38,7 @@ pub enum Cursor {
   ///Scroll the terminal one line down
   ScrollDown,
 
-  /*  FEATURE
-  ///Scroll the terminal one line right
-  ScrollRight,
-  ///Scroll the terminal one line left
-  ScrollLeft,
-      FEATURE  */
-
   ///----------- Erasing ------------
-  ///The output screen is cleared
-  Clear,
   ///Erase the current line from the cursor to the right border column (char under the cursor included)
   EraseRightLine,
   ///Erase the current line from the left border column to the cursor (char under the cursor included)
@@ -55,7 +49,22 @@ pub enum Cursor {
   EraseDown,
   ///Erase all lines from the current line up to the top of the screen, and erase the current line from the left border column to the cursor (char under the cursor included)
   EraseUp,
+  ///The output screen is cleared
+  Clear,
 
+
+  /* ****************** FEATURES ***********************
+  ///-- Save Cursor ---
+  --> Save multiple Cursor positions
+
+  ///----- Scroll -----
+  --> ///Scrolling enable flag for a definited part of the terminal (flag on/off, index of begin_line, index of end_line)
+    ScrollEnable(bool, libc::c_ushort, libc::c_ushort),
+  --> ///Scroll the terminal one line right
+    ScrollRight,
+  --> ///Scroll the terminal one line left
+    ScrollLeft,
+  ********************* FEATURES *********************** */
 
   /* *******  MACOS/iTerm2 WTF Facts *******
   --> <ESC>[5i Close the fd 0 instead of echoing input on a printer
