9b3dd3370bfe9b306b9e6f46cc920ea3cc93b5a7
adjivas <adjivas@users.noreply.github.com>
2016-09-28 17:04:02 +0200

    move child trait

--- src/fork/mod.rs
+++ src/fork/mod.rs
@@ -1,3 +1,23 @@
-mod child;
+use ::pty::prelude as pty;
+use ::libc;
 
-pub use self::child::Child;
+use std::ffi::CString;
+use std::ptr;
+
+pub trait Child {
+  fn exec(&self, &'static str) -> !;
+}
+
+impl Child for pty::Slave {
+  fn exec(&self, command: &'static str) -> ! {
+    let mut ptrs = [
+      CString::new(command).unwrap().as_ptr(),
+      ptr::null(),
+    ];
+
+    unsafe {
+      libc::execvp(*ptrs.as_ptr(), ptrs.as_mut_ptr());
+    }
+    unimplemented!();
+  }
+}
