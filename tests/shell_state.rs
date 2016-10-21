91ba3e189cf3a89b72b3b93eec2146ff108b2b02
adjivas <adjivas@users.noreply.github.com>
2016-10-21 20:16:36 +0200

    add interval

--- tests/shell_state.rs
+++ tests/shell_state.rs
@@ -11,7 +11,7 @@ const A: In = [b'a',
 ];
 
 const B: In = [b'b',
-            b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00'
+               b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00', b'\x00'
 ];
 
 #[test]
@@ -37,13 +37,46 @@ fn test_key_repeat() {
     state.set_input(None);
     state.set_input(Some(Control::new(A, 1)));
     assert_eq!(state.is_input_keyrepeat(), Some(2));
+    thread::sleep(time::Duration::from_millis(REPEAT as u64 -1));
+    state.set_input(None);
+    assert_eq!(state.is_input_keyrepeat(), Some(2));
     thread::sleep(time::Duration::from_millis(REPEAT as u64));
     state.set_input(None);
+    assert_eq!(state.is_input_keyrepeat(), None);
 
-    assert_eq!(state.is_input_keyrepeat(), Some(0));
     state.set_input(Some(Control::new(A, 1)));
     state.set_input(Some(Control::new(A, 1)));
     assert_eq!(state.is_input_keyrepeat(), Some(1));
     state.set_input(Some(Control::new(B, 1)));
     assert_eq!(state.is_input_keyrepeat(), Some(0));
+}
+
+#[test]
+fn test_key_interval() {
+    let mut state: ShellState = ShellState::new(None, Some(INTERVAL), libc::STDIN_FILENO);
+    state.set_input(Some(Control::new(A, 1)));
+    state.set_input(Some(Control::new(A, 1)));
+    state.set_input(Some(Control::new(A, 1)));
+
+    assert_eq!(state.is_input_keyrepeat(), Some(2));
+    assert_eq!(state.is_input_keyinterval(), Some(0));
+
+    thread::sleep(time::Duration::from_millis(REPEAT as u64 -1));
+    state.set_input(Some(Control::new(A, 1)));
+    assert_eq!(state.is_input_keyrepeat(), Some(3));
+    thread::sleep(time::Duration::from_millis(REPEAT as u64 -1));
+    state.set_input(Some(Control::new(A, 1)));
+    assert_eq!(state.is_input_keyrepeat(), Some(4));
+    assert_eq!(state.is_input_keyinterval(), Some(1));
+
+    thread::sleep(time::Duration::from_millis(REPEAT as u64 -1));
+    state.set_input(Some(Control::new(A, 1)));
+    state.set_input(Some(Control::new(A, 1)));
+    assert_eq!(state.is_input_keyrepeat(), Some(6));
+    assert_eq!(state.is_input_keyinterval(), Some(2));
+
+    thread::sleep(time::Duration::from_millis(REPEAT as u64));
+    state.set_input(None);
+    assert_eq!(state.is_input_keyrepeat(), None);
+    assert_eq!(state.is_input_keyinterval(), None);
 }
