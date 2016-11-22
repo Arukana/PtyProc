8b24bf4ae4c7470b0a8ef436a674407e23f7469d
Jeremy PEPIN <jpepin@e3r7p15.42.fr>
2016-11-22 02:01:05 +0100

    Add Some hard tests on redimension

--- src/shell/display/mod.rs
+++ src/shell/display/mod.rs
@@ -174,7 +174,7 @@ impl Display {
               { {0..size.ws_col-col}.all(|_|
                 { (*coucou).insert(((row - i) * col) as usize, Character::new(&[b' '][..]));
                   true }) }); }
-           // self.size = size;
+            self.size = size;
             let x = self.oob.0;
             let y = self.oob.1;
             self.goto_coord(x, y); }
@@ -191,7 +191,7 @@ impl Display {
               { {0..col-size.ws_col}.all(|k|
                 { (*coucou).remove((((row - i) * col) - (k + 1)) as usize);
                   true }) }); }
-      //      self.size = size;
+            self.size = size;
             let x = if self.oob.0 < size.ws_col as usize
             { self.oob.0 }
             else
--- tests/redimension.rs
+++ tests/redimension.rs
@@ -13,7 +13,7 @@ const SIZE: Winszed = Winszed
 
 
 #[test]
-//#[ignore] // For travis
+#[ignore] // For travis
 /// fn resize(&mut self) -> Result<()>
 fn test_redimension()
 { let mut display: Display = Display::from_winszed(SIZE);
@@ -154,13 +154,13 @@ vec![b'm', b' ', b'i', b'p', b's', b'u', b'm', b' ', b'd', b'o', b' ', b' ',
      b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ' ]);
 
   // Print "B"
-  assert_eq!(display.write(b"\rB").ok(), Some(1usize));
+  assert_eq!(display.write(b"B").ok(), Some(1usize));
   assert_eq!(display.into_bytes(),
 vec![b'm', b' ', b'i', b'p', b's', b'u', b'm', b' ', b'd', b'o', b' ', b' ',
      b'l', b'o', b'r', b' ', b's', b'i', b't', b' ', b'a', b'm', b' ', b' ',
      b'e', b't', b' ', b'h', b'e', b'l', b'l', b'o', b' ', b'l', b' ', b' ',
      b'o', b'r', b'e', b'm', b' ', b'i', b'p', b's', b'u', b'A', b' ', b' ',
-     b'B', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b'Q', b'B', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
      b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
      b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
      b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ' ]);
@@ -179,7 +179,7 @@ vec![b'm', b' ', b'i', b'p', b's', b'u', b'm', b' ', b'd', b'o', b' ', b' ',
            b'l', b'o', b'r', b' ', b's', b'i', b't', b' ',
            b'e', b't', b' ', b'h', b'e', b'l', b'l', b'o',
            b'o', b'r', b'e', b'm', b' ', b'i', b'p', b's',
-           b'B', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+           b'Q', b'B', b' ', b' ', b' ', b' ', b' ', b' ',
            b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
            b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
            b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ' ]);
@@ -191,11 +191,161 @@ vec![b'm', b' ', b'i', b'p', b's', b'u', b'm', b' ', b'd', b'o', b' ', b' ',
            b'l', b'o', b'r', b' ', b's', b'i', b't', b' ',
            b'e', b't', b' ', b'h', b'e', b'l', b'l', b'o',
            b'o', b'r', b'e', b'm', b' ', b'i', b'p', b's',
-           b'B', b'A', b' ', b' ', b' ', b' ', b' ', b' ',
+           b'Q', b'B', b'A', b' ', b' ', b' ', b' ', b' ',
            b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
            b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
            b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ' ]);
 
-  unsafe { libc::ioctl(0, libc::TIOCSWINSZ, &stock); }
-  unsafe { libc::ioctl(0, 536900721); }
-}
+  unsafe { libc::ioctl(0, libc::TIOCSWINSZ, &stock); }}
+
+#[test]
+//#[ignore] // For travis
+/// fn resize(&mut self) -> Result<()>
+fn hard_redimension()
+{ let mut display: Display = Display::from_winszed(SIZE);
+  assert_eq!(display.into_bytes(),
+      vec![b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+           b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+           b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+           b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+           b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+           b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+           b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+           b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ' ]);
+
+  // Stock Terminal size for the reset
+  let stock : Winszed = Winszed::default();
+
+  // Print "hello lorem ipsum dolor sit amet hello lorem ipsum dolor sit amet bonjour"
+  assert_eq!(display.write(b"hello lorem ipsum dolor sit amet hello lorem ipsum dolor sit amet bonjour").ok(), Some(73usize));
+  assert_eq!(display.into_bytes(),
+      vec![b'h', b'e', b'l', b'l', b'o', b' ', b'l', b'o', b'r', b'e',
+           b'm', b' ', b'i', b'p', b's', b'u', b'm', b' ', b'd', b'o',
+           b'l', b'o', b'r', b' ', b's', b'i', b't', b' ', b'a', b'm',
+           b'e', b't', b' ', b'h', b'e', b'l', b'l', b'o', b' ', b'l',
+           b'o', b'r', b'e', b'm', b' ', b'i', b'p', b's', b'u', b'm',
+           b' ', b'd', b'o', b'l', b'o', b'r', b' ', b's', b'i', b't',
+           b' ', b'a', b'm', b'e', b't', b' ', b'b', b'o', b'n', b'j',
+           b'o', b'u', b'r', b' ', b' ', b' ', b' ', b' ', b' ', b' ' ]);
+
+  let mut winsz = Winszed
+  { ws_row: 5,
+    ws_col: 7,
+    ws_xpixel: 0,
+    ws_ypixel: 0, };
+
+  // Remove 3 lines and 3 columns to the display
+  unsafe { libc::ioctl(0, libc::TIOCSWINSZ, &winsz); }
+  display.resize();
+  assert_eq!(display.into_bytes(),
+      vec![b'h', b'e', b'l', b'l', b'o', b' ', b'l',
+           b'm', b' ', b'i', b'p', b's', b'u', b'm',
+           b'l', b'o', b'r', b' ', b's', b'i', b't',
+           b'e', b't', b' ', b'h', b'e', b'l', b'l',
+           b'o', b'r', b'e', b'm', b' ', b'i', b'p' ]);
+
+  // Print "A"
+  assert_eq!(display.write(b"A").ok(), Some(1usize));
+  assert_eq!(display.into_bytes(),
+      vec![b'm', b' ', b'i', b'p', b's', b'u', b'm',
+           b'l', b'o', b'r', b' ', b's', b'i', b't',
+           b'e', b't', b' ', b'h', b'e', b'l', b'l',
+           b'o', b'r', b'e', b'm', b' ', b'i', b'A',
+           b' ', b' ', b' ', b' ', b' ', b' ', b' ' ]);
+
+  winsz = Winszed
+  { ws_row: 10,
+    ws_col: 12,
+    ws_xpixel: 0,
+    ws_ypixel: 0, };
+
+  // Add 5 lines and 5 columns to the display
+  unsafe { libc::ioctl(0, libc::TIOCSWINSZ, &winsz); }
+  display.resize();
+  assert_eq!(display.into_bytes(),
+vec![b'm', b' ', b'i', b'p', b's', b'u', b'm', b' ', b' ', b' ', b' ', b' ',
+     b'l', b'o', b'r', b' ', b's', b'i', b't', b' ', b' ', b' ', b' ', b' ',
+     b'e', b't', b' ', b'h', b'e', b'l', b'l', b' ', b' ', b' ', b' ', b' ',
+     b'o', b'r', b'e', b'm', b' ', b'i', b'A', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ' ]);
+
+  // Print "B"
+  assert_eq!(display.write(b"B").ok(), Some(1usize));
+  assert_eq!(display.into_bytes(),
+vec![b'm', b' ', b'i', b'p', b's', b'u', b'm', b' ', b' ', b' ', b' ', b' ',
+     b'l', b'o', b'r', b' ', b's', b'i', b't', b' ', b' ', b' ', b' ', b' ',
+     b'e', b't', b' ', b'h', b'e', b'l', b'l', b' ', b' ', b' ', b' ', b' ',
+     b'o', b'r', b'e', b'm', b' ', b'i', b'A', b' ', b' ', b' ', b' ', b' ',
+     b'B', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ', b' ' ]);
+
+  winsz = Winszed
+  { ws_row: 12,
+    ws_col: 5,
+    ws_xpixel: 0,
+    ws_ypixel: 0, };
+
+  // Add 2 lines and remove 7 columns to the display
+  unsafe { libc::ioctl(0, libc::TIOCSWINSZ, &winsz); }
+  display.resize();
+  assert_eq!(display.into_bytes(),
+vec![b'm', b' ', b'i', b'p', b's',
+     b'l', b'o', b'r', b' ', b's',
+     b'e', b't', b' ', b'h', b'e',
+     b'o', b'r', b'e', b'm', b' ',
+     b'B', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ' ]);
+
+  // Print "X"
+  assert_eq!(display.write(b"X").ok(), Some(1usize));
+  assert_eq!(display.into_bytes(),
+vec![b'm', b' ', b'i', b'p', b's',
+     b'l', b'o', b'r', b' ', b's',
+     b'e', b't', b' ', b'h', b'e',
+     b'o', b'r', b'e', b'm', b' ',
+     b'B', b'X', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ',
+     b' ', b' ', b' ', b' ', b' ' ]);
+
+  winsz = Winszed
+  { ws_row: 3,
+    ws_col: 10,
+    ws_xpixel: 0,
+    ws_ypixel: 0, };
+
+  // Remove 9 lines and add 5 columns to the display
+  unsafe { libc::ioctl(0, libc::TIOCSWINSZ, &winsz); }
+  display.resize();
+  assert_eq!(display.into_bytes(),
+      vec![b'm', b' ', b'i', b'p', b's', b' ', b' ', b' ', b' ', b' ',
+           b'l', b'o', b'r', b' ', b's', b' ', b' ', b' ', b' ', b' ',
+           b'e', b't', b' ', b'h', b'e', b' ', b' ', b' ', b' ', b' ' ]);
+
+  // Print "S"
+  assert_eq!(display.write(b"S").ok(), Some(1usize));
+  assert_eq!(display.into_bytes(),
+      vec![b'm', b' ', b'i', b'p', b's', b' ', b' ', b' ', b' ', b' ',
+           b'l', b'o', b'r', b' ', b's', b' ', b' ', b' ', b' ', b' ',
+           b'e', b't', b' ', b'h', b'S', b' ', b' ', b' ', b' ', b' ' ]);
+
+  unsafe { libc::ioctl(0, libc::TIOCSWINSZ, &stock); }}
