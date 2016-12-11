243c6a2d68b225b36295cf8461fdf316d5617a69
adjivas <adjivas@users.noreply.github.com>
2016-12-11 06:49:53 +0100

    Revert "Revert "Git revert""
    This reverts commit 5e2cd3fb5664e69533faae1474b047ba2d26f4b5.

--- src/shell/mod.rs
+++ src/shell/mod.rs
@@ -8,7 +8,6 @@ use std::os::unix::io::AsRawFd;
 use std::io::{self, Write};
 use std::mem;
 use std::fmt;
-use std::ffi;
 
 use ::libc;
 use ::fork::Child;
@@ -29,7 +28,7 @@ pub struct Shell {
     pid: libc::pid_t,
     #[allow(dead_code)]
     config: Termios,
-    speudo: File,
+    speudo: pty::Master,
     device: Device,
     state: ShellState,
     screen: Display,
@@ -53,90 +52,6 @@ impl Shell {
           libc::pipe(coucou);
           pipefd = Vec::from_raw_parts(coucou, 2, pipefd.capacity());
 
-<<<<<<< HEAD
-	  let pid = libc::fork();
-	  let coucou = libc::ttyname(0);
-	  let get = Vec::from_raw_parts(coucou as *mut u8, 1024, 1024);
-	  let name = get.iter().map(|&x| x as char).collect::<String>();
-	  let master = File::open(name.as_str()).unwrap();
-
-	  match pid
-	  { 0 =>
-              { // Child window init
-	        libc::ioctl(0, libc::TIOCSWINSZ, &winsz);
-	        // Use pipe
-	        libc::close(pipefd[0]);
-
-	        if cfg!(target_os = "macos")
-	        { // Max path length = 1024
-		  let mut the: Vec<u8> = Vec::with_capacity(1024);
-		  let mut bonjour = the.as_ptr() as *mut libc::c_void;
-
-		  // Get info about /dev/tty of the child
-		  libc::fcntl(libc::STDOUT_FILENO, 50, bonjour);
-
-                  // Transfer it to master
-                  libc::write(pipefd[1], bonjour, 1024); }
-                else if cfg!(any(target_os = "linux", target_os = "android"))
-		{ let mut path: String = String::with_capacity(1024);
-                  path.push_str("/proc/");
-                	  path.push_str(format!("{}", libc::getpid()).as_str());
-                  path.push_str("/fd/0");
-                  let mut get: Vec<u8> = Vec::with_capacity(1024);
-                  let mut buf = get.as_ptr() as *mut i8;
-                  let hey = path.as_ptr() as *const i8;
-                  libc::readlink(hey, buf, 1024);
-                  get = Vec::from_raw_parts(buf as *mut u8, 1024, get.capacity()); };
-
-                  libc::close(pipefd[1]);
-    let cmd = ffi::CString::new(command.unwrap_or("/bin/bash")).unwrap();
-    let mut args: Vec<*const libc::c_char> = Vec::with_capacity(1);
-
-    args.push(cmd.as_ptr());
-    args.push(ptr::null());
-    unsafe {
-      if libc::execvp(cmd.as_ptr(), args.as_mut_ptr()).eq(&-1) {
-        panic!("{}: {}", cmd.to_string_lossy(), ::errno::errno());
-      }
-    }
-    unreachable!(); },
-
-	    -1 => { unreachable!(); },
-            pid =>
-              { // Use pipe
-                libc::close(pipefd[1]);
-                let mut child_fd =
-		if cfg!(target_os = "macos")
-                { let mut get: Vec<u8> = Vec::with_capacity(1024);
-                  let mut buf = get.as_ptr() as *mut libc::c_void;
-
-                  // Receive the /dev/tty of the child
-                  libc::read(pipefd[0], buf, 1024);
-                  get = Vec::from_raw_parts(buf as *mut u8, 1024, get.capacity());
-                  let mut buf = get.as_ptr() as *const i8;
-
-                  // Collect the file desciptor of the child
-                  libc::open(buf, libc::O_RDWR) }
-                else if cfg!(any(target_os = "linux", target_os = "android"))
-		{ 0 }
-                else { 0 };
-
-                libc::close(pipefd[0]);
-
-                // Cas d'erreur si le fd est inférieur ou égal à 2
-                //assert!(child_fd.gt(&2));
-                if child_fd.le(&2)
-                { child_fd = 0; }
-
-                Ok(Shell
-                { child_fd: child_fd,
-                  pid: pid,
-                  config: Termios::default(),
-                  speudo: master,
-                  device: Device::from_speudo(master, libc::getpid()),
-                  state: ShellState::new(repeat, interval),
-                  screen: Display::new(libc::STDOUT_FILENO).unwrap(), })
-=======
           match pty::Fork::from_ptmx() {
               Err(cause) => Err(ShellError::ForkFail(cause)),
               Ok(fork) => match fork {
@@ -216,8 +131,8 @@ impl Shell {
                           state: ShellState::new(repeat, interval),
                           screen: Display::new(libc::STDOUT_FILENO).unwrap(),
                       })
->>>>>>> parent of 72e09f9... Ok display/mod for Linux
                   },
+              }
           }
       }}
 
