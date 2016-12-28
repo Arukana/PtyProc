2183952ca45913278498e283b63ced8358dae3cb
adjivas <adjivas@users.noreply.github.com>
2016-12-28 03:52:35 +0100

    fix control end of line

--- src/shell/device/control/mod.rs
+++ src/shell/device/control/mod.rs
@@ -12,81 +12,80 @@ use self::operate::Operate;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Control {
-  /// Buffer.
-  buf: In,
-  /// Length.
-  len: libc::size_t,
-  /// Time where the control was pressed.
-  time: time::Tm,
-  /// Operation.
-  operate: Operate,
+    /// Buffer.
+    buf: In,
+    /// Length.
+    len: libc::size_t,
+    /// Time where the control was pressed.
+    time: time::Tm,
+    /// Operation.
+    operate: Operate,
 }
 
 impl Control {
-  /// The constructor method `new` returns a Control's event from Device.
-  pub fn new(buf: In, len: libc::size_t) -> Self {
-    Control {
-      buf: buf,
-      len: len,
-      time: time::now(),
-      operate: Operate::new(&buf, len),
+    /// The constructor method `new` returns a Control's event from Device.
+    pub fn new(mut buf: In, len: libc::size_t) -> Self {
+        buf[len] = b'\0';
+        Control {
+            buf: buf,
+            len: len,
+            time: time::now(),
+            operate: Operate::new(&buf, len),
+        }
     }
-  }
 
-/*  pub fn clone_from_buf(&mut self, buf: In) {
-    self.buf.write(buf);
-  } */
+    /*  pub fn clone_from_buf(&mut self, buf: In) {
+        self.buf.write(buf);
+        } */
 
-  pub fn ss_mod(&mut self, ss: libc::c_uchar)
-  { self.buf = [b'\x1B', b'O', ss, 0, 0, 0, 0, 0, 0, 0, 0, 0]; } 
+    pub fn ss_mod(&mut self, ss: libc::c_uchar)
+    { self.buf = [b'\x1B', b'O', ss, 0, 0, 0, 0, 0, 0, 0, 0, 0]; } 
 
-  /// The accessor method `as_slice` returns the Control Event.
-  pub fn as_slice(&self) -> &[libc::c_uchar] {
-    &self.buf[..self.len]
-  }
-
-  /// The accessor method `as_time` returns the Time.
-  pub fn as_time(&self) -> time::Tm {
-    self.time
-  }
-
-  pub fn is_unicode(&self) -> Option<&[libc::c_uchar]> {
-    if self.operate.is_key().is_some() {
-      Some(self.as_slice())
-    } else {
-      None
+    /// The accessor method `as_slice` returns the Control Event.
+    pub fn as_slice(&self) -> &[libc::c_uchar] {
+        &self.buf[..self.len]
     }
-  }
 
-  /// The accessor method `is_enter` returns an Option for the Enter Key.
-  pub fn is_enter(&self) -> Option<()> {
-    match self.operate.is_key() {
-      Some(key) if key.is_enter() => Some(()),
-      _ => None,
+    /// The accessor method `as_time` returns the Time.
+    pub fn as_time(&self) -> time::Tm {
+        self.time
     }
-  }
 
-  /// The accessor method `is_key` returns an Option for
-  /// the Key interface.
-  pub fn is_key(&self) -> Option<Key> {
-    self.operate.is_key()
-  }
+    pub fn is_unicode(&self) -> Option<&[libc::c_uchar]> {
+        if self.operate.is_key().is_some() {
+            Some(self.as_slice())
+        } else {
+            None
+        }
+    }
 
-  /// The accessor method `is_mouse` returns an Option for
-  /// the Mouse interface.
-  pub fn is_mouse(&self) -> Option<(Mouse, libc::c_ushort, libc::c_ushort)> {
-    self.operate.is_mouse()
-  }
+    /// The accessor method `is_enter` returns an Option for the Enter Key.
+    pub fn is_enter(&self) -> Option<()> {
+        match self.operate.is_key() {
+            Some(key) if key.is_enter() => Some(()),
+            _ => None,
+        }
+    }
+
+    /// The accessor method `is_key` returns an Option for
+    /// the Key interface.
+    pub fn is_key(&self) -> Option<Key> {
+        self.operate.is_key()
+    }
+
+    /// The accessor method `is_mouse` returns an Option for
+    /// the Mouse interface.
+    pub fn is_mouse(&self) -> Option<(Mouse, libc::c_ushort, libc::c_ushort)> {
+        self.operate.is_mouse()
+    }
 }
 
 impl fmt::Display for Control {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    f.write_str(
-      unsafe {
-        str::from_utf8_unchecked(self.as_slice())
-      }
-    )
-  }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        unsafe {
+            f.write_str(str::from_utf8_unchecked(self.as_slice()))
+        }
+    }
 }
 
 impl PartialEq for Control {
--- src/shell/device/control/operate/mod.rs
+++ src/shell/device/control/operate/mod.rs
@@ -13,62 +13,62 @@ pub use self::err::{OperateError, Result};
 
 #[derive(Clone, Copy, Debug)]
 pub enum Operate {
-  /// The mouse operate.
-  Mouse(Mouse, libc::c_ushort, libc::c_ushort),
-  /// The key operate.
-  Key(Key),
+    /// The mouse operate.
+    Mouse(Mouse, libc::c_ushort, libc::c_ushort),
+    /// The key operate.
+    Key(Key),
 }
 
 impl Operate {
-  /// The constructor method `new` returns evaluated Operate.
-  pub fn new(buf: &In, len: libc::size_t) -> Self {
-    if let Ok(opt) = Operate::from_mouse(buf) {
-      opt
-    } else {
-      Operate::Key(Key::new(buf, len))
-    }
-  }
-
-  /// The constructor method `from_mouse` returns evaluated a mouse input.
-  pub fn from_mouse(buf: &[u8; 12]) -> Result<Self> {
-    match buf {
-      &[b'\x1B', b'[', b'<', action, b';', ref coordinate.., b'm'] |
-      &[b'\x1B', b'[', b'<', action, b';', ref coordinate.., b'M'] => {
-        match Mouse::new(action) {
-          Ok(cmd) => {
-            coordinate.iter().position(|&sep| sep.eq(&b';')).map(|index: usize| unsafe {
-              let (term_x, term_y): (&[u8], &[u8]) = coordinate.split_at(index);
-              let term_y: &[u8] = &term_y[1..{term_y.len()}];
-              if let (Ok(x), Ok(y)) = (
-                u16::from_str_radix(str::from_utf8_unchecked(term_x), 10),
-                u16::from_str_radix(str::from_utf8_unchecked(term_y), 10)
-              ) {
-                Ok(Operate::Mouse(cmd, x, y))
-              } else {
-                Err(OperateError::FromStrFail)
-              }
-            }).unwrap_or(Err(OperateError::PositionNotFound))
-          },
-          Err(why) => Err(OperateError::MouseFail(why)),
+    /// The constructor method `new` returns evaluated Operate.
+    pub fn new(buf: &In, len: libc::size_t) -> Self {
+        if let Ok(opt) = Operate::from_mouse(buf) {
+            opt
+        } else {
+            Operate::Key(Key::new(buf, len))
         }
-      },
-      _ => Err(OperateError::NotMouse),
     }
-  }
 
-  /// The accessor method `is_mouse` returns a Option for the Mouse Operate.
-  pub fn is_mouse(&self) -> Option<(Mouse, libc::c_ushort, libc::c_ushort)> {
-    match *self {
-      Operate::Mouse(mouse, x, y) => Some((mouse, x, y)),
-      Operate::Key(_) => None,
+    /// The constructor method `from_mouse` returns evaluated a mouse input.
+    pub fn from_mouse(buf: &[u8; 12]) -> Result<Self> {
+        match buf {
+            &[b'\x1B', b'[', b'<', action, b';', ref coordinate.., b'm'] |
+            &[b'\x1B', b'[', b'<', action, b';', ref coordinate.., b'M'] => {
+                match Mouse::new(action) {
+                    Ok(cmd) => {
+                        coordinate.iter().position(|&sep| sep.eq(&b';')).map(|index: usize| unsafe {
+                            let (term_x, term_y): (&[u8], &[u8]) = coordinate.split_at(index);
+                            let term_y: &[u8] = &term_y[1..{term_y.len()}];
+                            if let (Ok(x), Ok(y)) = (
+                                u16::from_str_radix(str::from_utf8_unchecked(term_x), 10),
+                                u16::from_str_radix(str::from_utf8_unchecked(term_y), 10)
+                            ) {
+                                Ok(Operate::Mouse(cmd, x, y))
+                            } else {
+                                Err(OperateError::FromStrFail)
+                            }
+                        }).unwrap_or(Err(OperateError::PositionNotFound))
+                    },
+                    Err(why) => Err(OperateError::MouseFail(why)),
+                }
+            },
+            _ => Err(OperateError::NotMouse),
+        }
     }
-  }
 
-  /// The accessor method `is_key` returns a Option for the Key Operate.
-  pub fn is_key(&self) -> Option<Key> {
-    match *self {
-      Operate::Key(key) => Some(key),
-      Operate::Mouse(_, _, _) => None,
+    /// The accessor method `is_mouse` returns a Option for the Mouse Operate.
+    pub fn is_mouse(&self) -> Option<(Mouse, libc::c_ushort, libc::c_ushort)> {
+        match *self {
+            Operate::Mouse(mouse, x, y) => Some((mouse, x, y)),
+            Operate::Key(_) => None,
+        }
     }
-  }
+
+    /// The accessor method `is_key` returns a Option for the Key Operate.
+    pub fn is_key(&self) -> Option<Key> {
+        match *self {
+            Operate::Key(key) => Some(key),
+            Operate::Mouse(_, _, _) => None,
+        }
+    }
 }
