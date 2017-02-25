f0f969bca804f4e0b8d201c898ef6a29fb8f2808
adjivas <adjivas@users.noreply.github.com>
2017-02-25 16:42:46 +0100

    desablee Iterator Item attribut Sized and Default

--- src/parent/mod.rs
+++ src/parent/mod.rs
@@ -8,7 +8,7 @@ use super::shell::display::winsz::Winszed;
 use super::shell::display::Display;
 use super::shell::state::ShellState;
 
-pub trait Parent : Default + Iterator<Item=ShellState> + io::Write + fmt::Display + fmt::Debug + Drop + Sized {
+pub trait Parent : Default + Iterator<Item=ShellState> + io::Write + fmt::Display + fmt::Debug + Drop {
     fn write_screen(&mut self, buf: &[u8]) -> io::Result<usize>;
     fn get_pid(&self) -> libc::pid_t; 
     fn get_speudo(&self) -> &pty::Master;
