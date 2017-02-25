4e3429f5fe5ba478c1ede1a6321358f4ced89598
adjivas <adjivas@users.noreply.github.com>
2017-02-25 10:13:18 +0100

    move write_screen to Parent

--- src/parent/mod.rs
+++ src/parent/mod.rs
@@ -8,6 +8,7 @@ use super::shell::display::winsz::Winszed;
 use super::shell::display::Display;
 
 pub trait Parent : Default + Iterator + io::Write + fmt::Display + fmt::Debug + Drop {
+    fn write_screen(&mut self, buf: &[u8]) -> io::Result<usize>;
     fn get_pid(&self) -> libc::pid_t; 
     fn get_speudo(&self) -> &pty::Master;
     fn get_screen(&self) -> &Display;
